6867b868bb9422289516a256fdde0aeae25dc06c
Matthew Masiello <home@Matthews-MBP.fios-router.home>
2019-01-29 21:00:49 -0500

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,24 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
+    self.end_with?("?")
 
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    if self.split(/[\!\?\.]/).include?("")
+      self.split(/[\!\?\.]/).length - self.split(/[\!\?\.]/).count("")
+    else
+      self.split(/[\!\?\.]/).length
+    end
   end
+
 end
